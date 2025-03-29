034e3774e89c91ef88e7b62275a0259709ebd475
eldadstar <eldadf456@gmail.com>
2025-03-29 13:51:13 +0300

    Admin Panel

---
+++ admin/lib/views/main_screen.dart
@@ -0,0 +1,99 @@
+import 'package:admin/views/side_bar_screens/buyers_screen.dart';
+import 'package:admin/views/side_bar_screens/category_screen.dart';
+import 'package:admin/views/side_bar_screens/orders_screen.dart';
+import 'package:admin/views/side_bar_screens/product_screen.dart';
+import 'package:admin/views/side_bar_screens/upload_banner.dart';
+import 'package:admin/views/side_bar_screens/vendors_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_admin_scaffold/admin_scaffold.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  Widget _selectedScreen = VendorsScreen();
+
+  screenSelector(item){
+    switch(item.route){
+      case BuyersScreen.id:
+        setState(() {
+          _selectedScreen = BuyersScreen();
+        });
+
+        break;
+
+    case VendorsScreen.id:
+       setState(() {
+         _selectedScreen = VendorsScreen();
+       });
+       break;
+    
+    case OrdersScreen.id:
+       setState(() {
+         _selectedScreen = OrdersScreen();
+       });
+       break;
+    
+    case CategoryScreen.id:
+      setState(() {
+        _selectedScreen = CategoryScreen();
+      });
+      break;
+
+    case UploadBanner.id:
+       setState(() {
+         _selectedScreen = UploadBanner();
+       });
+       break;
+    
+    case ProductScreen.id:
+       setState(() {
+         _selectedScreen = ProductScreen();
+       });
+    }  
+
+    
+  }
+  @override
+  Widget build(BuildContext context) {
+    return AdminScaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue, 
+        title: const Text("Management")
+      ),
+      body: _selectedScreen,
+      sideBar:  SideBar(
+        header: Container(
+          height: 50,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Colors.purple
+          ),
+          child: Center(child: Text("Ecommerce Store", style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 1.7,
+            color: Colors.white
+          ),),),
+        ),
+        items: const [
+          AdminMenuItem(title: 'Vendors', route: VendorsScreen.id, icon: CupertinoIcons.person_3),
+          AdminMenuItem(title: 'Buyers', route: BuyersScreen.id, icon: CupertinoIcons.person),
+          AdminMenuItem(title: 'Orders', route: OrdersScreen.id, icon: CupertinoIcons.shopping_cart),
+          AdminMenuItem(title: 'Categories', route: CategoryScreen.id, icon: Icons.category),
+          AdminMenuItem(title: 'Upload Banners', route: UploadBanner.id, icon: Icons.upload),
+          AdminMenuItem(title: 'Products', route: ProductScreen.id, icon: Icons.store),
+        ], 
+        selectedRoute: VendorsScreen.id,
+        onSelected: (item){
+          screenSelector(item);
+        },
+        ),
+      );
+  }
+}
---
+++ admin/lib/views/side_bar_screens/upload_banner.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class UploadBanner extends StatelessWidget {
+  static const String id = '\banner-screen';
+  const UploadBanner({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('upload Banner'),
+    );
+  }
+}
