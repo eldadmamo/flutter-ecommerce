23d776eaee9748619cb66a0c16bdff33db618131
eldadstar <eldadf456@gmail.com>
2025-03-29 15:04:50 +0300

    Category section Admin panel

--- admin/lib/views/side_bar_screens/category_screen.dart
+++ admin/lib/views/side_bar_screens/category_screen.dart
@@ -1,13 +1,170 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
-class CategoryScreen extends StatelessWidget {
+class CategoryScreen extends StatefulWidget {
   static const String id = '\categoryScreen';
   const CategoryScreen({super.key});
 
   @override
+  State<CategoryScreen> createState() => _CategoryScreenState();
+}
+
+class _CategoryScreenState extends State<CategoryScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  late String categoryName;
+  dynamic _bannerImage;
+  dynamic _image;
+   pickImage() async{
+   FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+      allowMultiple: false 
+    );
+
+    if (result!= null){
+      setState(() {
+        _image = result.files.first.bytes;
+      });
+    }
+   }
+
+   pickBannerImage() async{
+   FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+      allowMultiple: false 
+    );
+
+    if (result!= null){
+      setState(() {
+        _bannerImage = result.files.first.bytes;
+      });
+    }
+   } 
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Category Screen"),
+    return Form( 
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              alignment: Alignment.topLeft,
+              child: Text('Categories', 
+              style: TextStyle(
+                fontSize: 36, 
+                fontWeight: FontWeight.bold
+                ),
+                ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Divider(
+              color: Colors.grey
+            ),
+          ),
+          Row(
+            children: [
+              Container(
+                width: 150, 
+                height: 150,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(5,), 
+                ),
+                child: Center(
+                  child: _image!=null 
+                  ? Image.memory(_image)
+                  : Text('Category image'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 150, 
+                  child: TextFormField(
+                    onChanged: (value){
+                      categoryName=value;
+                    },
+                    validator: (value){
+                      if(value!.isNotEmpty){
+                        return null;
+                      } else {
+                        return 'Please enter category name';
+                      }
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'Enter Category name'
+                    )
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: (){
+                  if(_formKey.currentState!.validate()){
+                   
+                  }
+                }, 
+                child: Text('cancel')
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue
+                ),
+                onPressed: (){
+                  if (_formKey.currentState!.validate()){
+                     print(categoryName); 
+                  }
+                }, child: Text(
+                'Save',
+                style: TextStyle(
+                  color: Colors.white
+                ),
+                ),
+                ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: (){
+                pickImage();
+              }, 
+              child: Text("Pick Image")),
+          ),
+          Divider(
+            color: Colors.black,
+          ),
+          Container(
+                width: 150, 
+                height: 150,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(5,), 
+                ),
+                child: Center(
+                  child: _bannerImage!=null 
+                  ? Image.memory(_bannerImage) 
+                  : const Text(
+                    'Category Banner', 
+                    style: TextStyle(
+                      color: Colors.white),
+                    ),
+                ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(onPressed: (){
+              pickBannerImage();
+            }, child: Text('Pick Image')),
+          ),
+           Divider(
+            color: Colors.black,
+          ),
+        ],
+      ),
     );
   }
 }
