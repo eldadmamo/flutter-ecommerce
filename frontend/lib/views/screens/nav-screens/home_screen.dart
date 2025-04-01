edb3a76f389002d6cfba0d98fd3969bbcf170cfc
eldadstar <eldadf456@gmail.com>
2025-04-01 17:01:09 +0300

    Banner Display

--- frontend/lib/views/screens/nav-screens/home_screen.dart
+++ frontend/lib/views/screens/nav-screens/home_screen.dart
@@ -1,5 +1,6 @@
 
 
+import 'package:ecommerceflutter/views/screens/nav-screens/widgets/banner_widget.dart';
 import 'package:ecommerceflutter/views/screens/nav-screens/widgets/header-widget.dart';
 import 'package:flutter/material.dart';
 
@@ -13,6 +14,7 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           children: [
             HeaderWidget(),
+            BannerWidget()
           ],
         ),
       ),
