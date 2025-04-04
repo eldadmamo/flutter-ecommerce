4c188062d55eab27e1542037162426aac7b847a6
eldadstar <eldadf456@gmail.com>
2025-04-05 01:58:37 +0300

    Backend for Customer Orders

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -20,6 +20,25 @@ orderRouter.post('/api/orders', async (req, res) => {
     }
 })
 
+//Get Route for fetching 
+
+orderRouter.get('/api/orders/:buyerId', async (req, res) => {
+    try{
+        //extract the buyer from request paramerts
+        const {buyerId} =req.params;
+        const orders = await Order.find({buyerId});
+
+        if(orders===0){
+            return res.status(404).json({msg: "No Orders found for this buyer"});
+        }
+        //If orders are found, return them with a 200 status code
+        return res.status(200).json(orders);
+    }catch(error){
+        //Handle any errors that occure during the order retrieval process
+        res.status(500).json({error: e.message});
+    }
+})
+
 module.exports = orderRouter;
 
 
