efbdc444baec00154f31c9b1a22b36b83d3d4aff
eldadstar <eldadf456@gmail.com>
2025-04-07 17:14:32 +0300

    Advanced Wishlist  Data Persistence with SharedPreferences(in detail)

--- frontend/lib/provider/favorite_provider.dart
+++ frontend/lib/provider/favorite_provider.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:ecommerceflutter/models/favorite.dart';
 import 'package:ecommerceflutter/provider/cart_provider.dart';
 import 'package:ecommerceflutter/provider/category_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 
 final favoriteProvider = 
@@ -12,7 +15,39 @@ final favoriteProvider =
 );
 
 class FavoriteNotifier extends StateNotifier<Map<String, Favorite>>{
-  FavoriteNotifier():super({});
+  FavoriteNotifier():super({}){
+    _loadFavorites();
+  }
+
+  // a private method that loads items from sharedpreferences 
+  Future<void> _loadFavorites() async{
+    final prefs = await SharedPreferences.getInstance();
+    // fetch the json string of the favoirte items from sharedpreferneces under the key favorite 
+    final favoriteString = prefs.getString('favorites');
+    // check if the string is not null, there is saved data to load
+    if(favoriteString!=null){
+      //decode the json string to map in dymanic data
+     final Map<String,dynamic> favoriteMap = jsonDecode(favoriteString);
+
+     // convert the dynamic into a map of Favorite Object using the "fromjson" factory method
+     final favorites = favoriteMap.map((key,value)=> 
+     MapEntry(key, Favorite.fromJson(value)));
+
+     //updating the staus with the loaded favorites
+     state = favorites;
+
+     
+    }
+  }
+  // A private method that saves the current list of favorite items to shared preferences
+  Future<void> _saveFavorite()async{
+    final prefs = await SharedPreferences.getInstance();
+    //encoding the current state (Map of favorite object) into json String
+    final favoriteString = jsonEncode(state);
+    //saving the json string to sharedpreferences with the key "favorites"
+    await prefs.setString('favorites', favoriteString);
+
+  }
 
   void addProductToFavorite({
   required String productName,
@@ -40,12 +75,14 @@ class FavoriteNotifier extends StateNotifier<Map<String, Favorite>>{
     );
     // notify listeners the state has changes
     state = {...state};
+    _saveFavorite();
   }
 
   void removeCartItem(String productId){
     state.remove(productId);
     //notify listeners that the state has changed
     state = {...state};
+    _saveFavorite();
   }
 
   Map<String, Favorite> get getFavoriteItem => state;
